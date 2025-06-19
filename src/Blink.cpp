2acff13a06590c665bb7ea761eac8c288764bfb1
zjw939057120 <zjw939057120@outlook.com>
2025-06-19 20:30:55 +0800

    RGB灯带

--- src/Blink.cpp
+++ src/Blink.cpp
@@ -5,25 +5,61 @@
  */
 
 #include <Arduino.h>
+#include <STM32FreeRTOS.h>
+#include <Adafruit_NeoPixel.h>
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN PC13
+#ifdef ARDUINO_GENERIC_STM32F103RC
+#define LED_BUILTIN PC7
 #endif
 
-void setup()
+#define PIN PA8
+#define NUMPIXELS 32
+
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+#define DELAYVAL 500
+
+void TaskBlink(void *pvParameters)
 {
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+
+  for (;;)
+  {
+    // turn the LED on (HIGH is the voltage level)
+    digitalWrite(LED_BUILTIN, HIGH);
+    // wait for a second
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // turn the LED off by making the voltage LOW
+    digitalWrite(LED_BUILTIN, LOW);
+    // wait for a second
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+void TaskNeoPixel(void *pvParameters)
+{
+  pixels.begin();
+
+  for (;;)
+  {
+    pixels.clear();
+    for (int i = 0; i < NUMPIXELS; i++)
+    {
+      pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+      pixels.show();
+      vTaskDelay(100 / portTICK_PERIOD_MS);
+    }
+  }
+}
+
+void setup()
+{
+  xTaskCreate(TaskBlink, "Blink", 128, NULL, 1, NULL);
+  xTaskCreate(TaskNeoPixel, "NeoPixel", 128, NULL, 2, NULL);
+  vTaskStartScheduler();
 }
 
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-  // wait for a second
-  delay(1000);
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-  // wait for a second
-  delay(1000);
+  // Empty. Things are done in Tasks.
 }
