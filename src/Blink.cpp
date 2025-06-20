378f0bd005e40be8c755d0f57433dc4703946827
zjw939057120 <zjw939057120@outlook.com>
2025-06-20 20:21:24 +0800

    串口支持

--- src/Blink.cpp
+++ src/Blink.cpp
@@ -9,8 +9,14 @@
 #include <Adafruit_NeoPixel.h>
 
 #ifdef ARDUINO_GENERIC_STM32F103RC
-#define LED_BUILTIN PC7
+#define LED_BUILTIN_1 PC7
+#define LED_BUILTIN_2 PC6
+#elif ARDUINO_GENERIC_STM32F103VE
+#define LED_BUILTIN_1 PB14
+#define LED_BUILTIN_2 PB13
 #endif
+
+#define CAN_BUFFER_LEN 13
 
 #define PIN PA8
 #define NUMPIXELS 32
@@ -21,16 +27,18 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 void TaskBlink(void *pvParameters)
 {
   // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN_1, OUTPUT);
+  pinMode(LED_BUILTIN_2, OUTPUT);
 
   for (;;)
   {
-    // turn the LED on (HIGH is the voltage level)
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(LED_BUILTIN_1, HIGH);
+    digitalWrite(LED_BUILTIN_2, LOW);
     // wait for a second
     vTaskDelay(1000 / portTICK_PERIOD_MS);
-    // turn the LED off by making the voltage LOW
-    digitalWrite(LED_BUILTIN, LOW);
+
+    digitalWrite(LED_BUILTIN_1, LOW);
+    digitalWrite(LED_BUILTIN_2, HIGH);
     // wait for a second
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
@@ -54,6 +62,9 @@ void TaskNeoPixel(void *pvParameters)
 
 void setup()
 {
+  Serial.begin(115200);
+  Serial.setTimeout(5); // 设置串口接收超时时间为5ms
+
   xTaskCreate(TaskBlink, "Blink", 128, NULL, 1, NULL);
   xTaskCreate(TaskNeoPixel, "NeoPixel", 128, NULL, 2, NULL);
   vTaskStartScheduler();
@@ -61,5 +72,10 @@ void setup()
 
 void loop()
 {
-  // Empty. Things are done in Tasks.
+  uint8_t buffer[CAN_BUFFER_LEN] = {0};
+  if (Serial.available() > 0)
+  {
+    size_t len = Serial.readBytes(buffer, CAN_BUFFER_LEN);
+    Serial.write(buffer, CAN_BUFFER_LEN);
+  }
 }
